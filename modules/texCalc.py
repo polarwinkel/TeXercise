f37aca213d5e06c71f2f372af9df9f459365f49b
polarwinkel <it@polarwinkel.de>
2022-05-02 22:02:25 +0200

    added support for round brackets in formulas and `sqrt()`

--- modules/texCalc.py
+++ modules/texCalc.py
@@ -12,16 +12,38 @@ All it can do for now is:
 make sure you always use *, xy != x*y , in that case xy would be a value!
 
 TODO:
-- implement brackets ()
+- ~~implement brackets ()~~ done
 - implement \frac{}{}
 - implement \cdot
-- implement \sqrt{}
+- implement \sqrt{} (works now with round brackets)
 - implement ^
 - implement default values for pi and e that the user can override
 '''
 
-def texCalc(formula, values):
+import math
+
+def texCalc(formula, values=''):
     ''' claculate the formula recursively with the given values '''
+    # sqrt:
+    if formula.strip().startswith('\sqrt'):
+        return str(math.sqrt(float(texCalc(formula[5:], values))))
+    # brackets:
+    if formula.strip().startswith('('):
+        n=1
+        formula=formula[1:]
+        part1 = ''
+        while len(formula)>0:
+            if formula.startswith('('):
+                n=n+1
+            elif formula.startswith(')'):
+                n=n-1
+            if n==0:
+                formula=formula[1:]
+                break
+            part1=part1+formula[0]
+            formula=formula[1:]
+        formula = str(texCalc(part1, values))+formula
+    # arithmetic operations:
     if formula.partition('*')[2] != '':
         a = formula.partition('*')[0]
         b = formula.partition('*')[2]
